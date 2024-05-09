4c0a166f5091b121a44b209df2388a2f3e8f0316
your_username <royadaneshi2001@gmail.com>
2024-05-09 14:12:25 +0330

    uunifast implementation

--- main.py
+++ main.py
@@ -28,10 +28,22 @@ def uunifast(num_tasks, total_utilization):
     return tasks
 
 
+# this function returns task set for each total utilization in dictionary
+def get_task_by_utilization():
+    num_task = 50
+    utility_task = {}
+    total_utilization = [0.25, 0.5, 0.3, 0.7]
+    for u in total_utilization:
+        tasks = uunifast(num_task, u)
+        utility_task[str(u)] = tasks
+    return utility_task
+
+
 if __name__ == '__main__':
-    num_tasks = 5
-    total_utilization = 0.75
-    tasks = uunifast(num_tasks, total_utilization)
+
+    u_tasks = get_task_by_utilization()
+    # example of usage:
+    tasks = u_tasks["0.25"]
     for task in tasks:
         print(
             f"Task {task['task_id']}: Execution Time = {task['execution_time']:.2f}, Deadline = {task['deadline']}, Period = {task['period']}, Utilization = {task['utilization']:.2f}, Priority = {task['priority']}")
