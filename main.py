c4218f0fa21db617820775a385204f7874362068
your_username <royadaneshi2001@gmail.com>
2024-05-28 11:11:52 +0330

    shedulable plot

--- main.py
+++ main.py
@@ -22,11 +22,7 @@ class Task:
         self.successors = []
         self.core = None
 
-        # QoS metrics
-        self.latency = 0
-        self.jitter = 0
-        self.execution_start_time = None
-        self.execution_end_time = None
+
 
     @staticmethod
     def get_task(id):
@@ -125,8 +121,7 @@ def least_laxity_first(cores):
                 QoS += slack / current_time
                 QoS = QoS % 1
 
-        if min_laxity_task.execution_start_time is None:
-            min_laxity_task.execution_start_time = current_time
+
 
         print(f"Executing Task {min_laxity_task.id} on Core {min_laxity_task.core} at time {current_time}")
         min_laxity_task.remaining_execution_time -= 1
@@ -140,48 +135,59 @@ def least_laxity_first(cores):
 
 
 def scheduability_plot():
-    schedulable_counter1 = 0
-    schedulable_counter2 = 0
-    schedulable_counter3 = 0
-    schedulable_counter4 = 0
-    for i in range(0, 100):
-        num_tasks = 50
-        total_utilization = 0.25
-        tasks = generate_non_periodic_tasks(num_tasks, total_utilization)
-        cores1 = assign_tasks_to_cores(tasks, 16)
-        _, _, schuable1 = least_laxity_first(cores1)
-        if schuable1:
-            schedulable_counter1 += 1
-        total_utilization = 0.5
-        tasks2 = generate_non_periodic_tasks(num_tasks, total_utilization)
-        cores = assign_tasks_to_cores(tasks2, 16)
-        _, _, schuable2 = least_laxity_first(cores)
-        if schuable2:
-            schedulable_counter2 += 1
-        total_utilization = 0.3
-        tasks2 = generate_non_periodic_tasks(num_tasks, total_utilization)
-        cores = assign_tasks_to_cores(tasks2, 32)
-        _, _, schuable3 = least_laxity_first(cores)
-        if schuable3:
-            schedulable_counter3 += 1
-        total_utilization = 0.7
-        tasks2 = generate_non_periodic_tasks(num_tasks, total_utilization)
-        cores = assign_tasks_to_cores(tasks2, 32)
-        _, _, schuable4 = least_laxity_first(cores)
-        if schuable4:
-            schedulable_counter4 += 1
-
-    scheduales = [schedulable_counter1, schedulable_counter4, schedulable_counter3, schedulable_counter4]
-    print(scheduales)
-    plt.figure(figsize=(24, 18))
-    plt.bar(np.arange(4), scheduales, width=0.5)
-    plt.xlabel('system types')
-    plt.ylabel('number of tasks')
-    plt.title('number of schedulable task sets over 100')
-    plt.legend()
-
-    plt.tight_layout()
-    plt.show()
+    num_tasks = 4
+    total_utilization = 0.25
+    tasks = generate_non_periodic_tasks(num_tasks, total_utilization)
+    for ta in tasks:
+        print(ta.id, ta.execution_time, ta.deadline, ta.release_time)
+        for p in ta.predecessors:
+            print(p.id)
+    cores1 = assign_tasks_to_cores(tasks, 16)
+    _, _, schuable1 = least_laxity_first(cores1)
+    if schuable1:
+        print("yes")
+    # schedulable_counter1 = 0
+    # schedulable_counter2 = 0
+    # schedulable_counter3 = 0
+    # schedulable_counter4 = 0
+    # for i in range(0, 100):
+    #     num_tasks = 4
+    #     total_utilization = 0.25
+    #     tasks = generate_non_periodic_tasks(num_tasks, total_utilization)
+    #     cores1 = assign_tasks_to_cores(tasks, 16)
+    #     _, _, schuable1 = least_laxity_first(cores1)
+    #     if schuable1:
+    #         schedulable_counter1 += 1
+    #     total_utilization = 0.5
+    #     tasks2 = generate_non_periodic_tasks(num_tasks, total_utilization)
+    #     cores = assign_tasks_to_cores(tasks2, 16)
+    #     _, _, schuable2 = least_laxity_first(cores)
+    #     if schuable2:
+    #         schedulable_counter2 += 1
+    #     total_utilization = 0.3
+    #     tasks2 = generate_non_periodic_tasks(num_tasks, total_utilization)
+    #     cores = assign_tasks_to_cores(tasks2, 32)
+    #     _, _, schuable3 = least_laxity_first(cores)
+    #     if schuable3:
+    #         schedulable_counter3 += 1
+    #     total_utilization = 0.7
+    #     tasks2 = generate_non_periodic_tasks(num_tasks, total_utilization)
+    #     cores = assign_tasks_to_cores(tasks2, 32)
+    #     _, _, schuable4 = least_laxity_first(cores)
+    #     if schuable4:
+    #         schedulable_counter4 += 1
+    #
+    # scheduales = [schedulable_counter1, schedulable_counter4, schedulable_counter3, schedulable_counter4]
+    # print(scheduales)
+    # plt.figure(figsize=(24, 18))
+    # plt.bar(np.arange(4), scheduales, width=0.5)
+    # plt.xlabel('system types')
+    # plt.ylabel('number of tasks')
+    # plt.title('number of schedulable task sets over 100')
+    # plt.legend()
+    #
+    # plt.tight_layout()
+    # plt.show()
 
 
 if __name__ == '__main__':
